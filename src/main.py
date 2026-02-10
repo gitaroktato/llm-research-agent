089d94fc5dd2a6c7d0ffa352be3144137980f0f8
gitaroktato <oresztesz_margaritisz@epam.com>
2026-02-10 15:23:58 +0100

    feat: Integrating Langfuse

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
+from langfuse import get_client
+from langfuse.langchain import CallbackHandler
 from deepagents import create_deep_agent
 from langchain_mcp_adapters.client import MultiServerMCPClient
+
+# Initialize Langfuse client
+langfuse = get_client()
+
+# Verify connection
+if langfuse.auth_check():
+    print("Langfuse client is authenticated and ready!")
+else:
+    print("Authentication failed. Please check your credentials and host.")
+
+# Initialize Langfuse CallbackHandler for LangChain (tracing)
+langfuse_handler = CallbackHandler()
 
 
 async def make_agent():
@@ -14,4 +28,4 @@ async def make_agent():
     )  # pyright: ignore
     tools = await client.get_tools()
     agent = create_deep_agent(model="azure_openai:gpt-4o-mini-2024-07-18", tools=tools)
-    return agent
+    return agent.with_config(config={"callbacks": [langfuse_handler]})
