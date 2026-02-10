d1208b709a2f03a5d659d4dcd75bf11a9915be1f
gitaroktato <oresztesz_margaritisz@epam.com>
2026-02-10 13:09:35 +0100

    refactor: Code organizing

--- src/main.py
+++ src/main.py
@@ -1,32 +1,5 @@
-from langchain_mcp_adapters.sessions import Connection
-from langgraph.graph.state import CompiledStateGraph
-from mcp.client.session import ClientSession
-from mcp.client.stdio import StdioServerParameters, stdio_client
-from langchain_mcp_adapters.tools import load_mcp_tools
-from langchain_mcp_adapters.client import MultiServerMCPClient, StdioConnection
-
 from deepagents import create_deep_agent
-
-server_params = StdioServerParameters(
-    command="npx",
-    args=["-y", "reddit-mcp-buddy"],
-)
-
-agent: CompiledStateGraph
-
-
-async def run_agent():
-    async with stdio_client(server_params) as (read, write):
-        async with ClientSession(read, write) as session:
-            # Initialize the connection
-            await session.initialize()
-
-            # Get tools
-            tools = await load_mcp_tools(session)
-            agent = create_deep_agent(
-                model="azure_openai:gpt-4o-mini-2024-07-18", tools=tools
-            )
-            return agent
+from langchain_mcp_adapters.client import MultiServerMCPClient
 
 
 async def make_agent():
